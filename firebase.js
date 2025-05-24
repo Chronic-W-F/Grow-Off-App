0667ac64e09b59fc3c4ff5ba41a1452c358d3790
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-24 16:27:42 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAt5pe1CX9rfdQjJvUuaVrIEf-VbnyrTSw",
+  authDomain: "grow-off-app.firebaseapp.com",
+  projectId: "grow-off-app",
+  storageBucket: "grow-off-app.firebasestorage.app",
+  messagingSenderId: "250665554645",
+  appId: "1:250665554645:web:f8dd94ee4dc187a0aa885c",
+  measurementId: "G-HVLGY64YLQ"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
